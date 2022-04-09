e9aeb47c9e78334682fee4bfc09ab37257603b93
Dahiver Guerra <roddavid1@gmail.com>
2022-04-09 19:05:44 -0400

    added more info to the help class

--- src/main/java/me/dahiverguerra/Commands/Help.java
+++ src/main/java/me/dahiverguerra/Commands/Help.java
@@ -18,12 +18,15 @@ public class Help extends ListenerAdapter {
             help.setTitle("🤖 Help", null);
             help.setDescription("Commands for the Studious Fellow bot.");
             help.setColor(new Color(0xE3FFA3));
-            help.addField("!info", "Gives information about the Studious Fellow bot.", false);
-            help.addField("!help", "List of commands for the bot.", false);
-            help.addField("!meme", "Drops some fresh memes from reddit.", false);
-            help.addField("!invite", "Allows Studious Fellow to pass out some croissants (invites) to your server.", false);
-            help.addField("!study", "Gives user instructions on how to use the 25 min. Pomodoro timer.", false);
-            help.addField("!start", "Starts the timer, enabling the study session.", false);
+            help.setImage("https://media2.giphy.com/media/WoWm8YzFQJg5i/giphy.gif");
+            help.addField("`!info`", "*Gives information about the Studious Fellow bot.*", false);
+            help.addField("`!help`", "*List of commands for the bot.*", false);
+            help.addField("`!meme`", "*Drops some fresh memes from reddit.*", false);
+            help.addField("`!invite`", "*Allows Studious Fellow to pass out some croissants (invites) to your server.*", false);
+            help.addField("`!study`", "*Gives user instructions on how to use the 25 min. Pomodoro timer.*", false);
+            help.addField("`!start`", "*Starts the timer, enabling the study session.*", false);
+            help.addField("`!cf`", "*Flips a coin in style.*", false);
+            help.addField("`!fcookie`", "*Open up your very own fortune cookie.*", false);
             event.getChannel().sendMessageEmbeds(help.build()).queue();
             help.clear();
         }
