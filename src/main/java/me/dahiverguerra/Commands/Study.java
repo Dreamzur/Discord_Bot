d615da1f6cd1658adc0bfb88b327a37004137847
Dahiver Guerra <roddavid1@gmail.com>
2022-04-09 19:13:58 -0400

    fixed study section

--- src/main/java/me/dahiverguerra/Commands/Study.java
+++ src/main/java/me/dahiverguerra/Commands/Study.java
@@ -20,7 +20,9 @@ public class Study extends ListenerAdapter {
             study.setImage("https://www.ntaskmanager.com/wp-content/uploads/2019/05/best-pomodoro-apps-blog-header-main-1.png");
             study.setColor(new Color(0xaba4c8));
             study.addField("!start", "This command starts the timer!", true);
-            study.addField("!stop", "This command stops the timer!", true);
+            study.setFooter( "*Timer will run continuously for 25 minutes unless it implodes." +
+                    " The bot will also let you know when the timer is halfway through those 25 minutes!" + "\n **For" +
+                    " presentation purposes the timer will run for 1 minute and set off a remainder at 30 seconds.");
             event.getChannel().sendMessageEmbeds(study.build()).queue();
             study.clear();
         }
