430658ef9031e4b58082a2f3a32ba46a2bbd7d46
Dahiver Guerra <roddavid1@gmail.com>
2022-04-09 20:18:49 -0400

    added a clear command / added gatewayintent

---
+++ src/main/java/me/dahiverguerra/Commands/Clear.java
@@ -0,0 +1,40 @@
+package me.dahiverguerra.Commands;
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.List;
+
+import static me.dahiverguerra.DiscordBot.prefix;
+
+public class Clear extends ListenerAdapter {
+
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+        String[] args = event.getMessage().getContentRaw().split(" ");
+
+        if (args[0].equalsIgnoreCase(prefix + "clear")) {
+            if (args.length != 2) {
+                return;
+            }
+            // if args 1 is not a number, return event message
+            if (!isNumber(args[1])) {
+                event.getChannel().sendMessage("!clear amount").queue();
+                return;
+            }
+        }
+
+        List<Message> messageList = event.getChannel().getHistory().retrievePast(Integer.parseInt(args[1]) + 1).complete();
+        event.getTextChannel().deleteMessages(messageList).queue();
+    }
+
+    private boolean isNumber(String msg) {
+        try {
+            Integer.parseInt(msg); //try to convert message to int
+            return true;
+        } catch (NumberFormatException e) {
+            return false; //otherwise, it'll catch error and return false
+        }
+    }
+}
--- src/main/java/me/dahiverguerra/DiscordBot.java
+++ src/main/java/me/dahiverguerra/DiscordBot.java
@@ -4,19 +4,27 @@ import me.dahiverguerra.Commands.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
+import java.util.Arrays;
 
 public class DiscordBot {
     public static JDA bot;
     public static String prefix = "!";
+    public static final GatewayIntent[] intents = {GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_MESSAGES,
+            GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_MEMBERS,
+            GatewayIntent.GUILD_PRESENCES};
 
 
     public static void main(String[] args) throws LoginException {
 
 
+
         //Initializing the bot
-        bot = JDABuilder.createDefault("OTU5NjEzNjQ1MzIxNTM1NDk4.Ykeb2Q.lKUTKAduDO_q1n1FA_qOrAEe1zM")
+        bot = JDABuilder.createDefault("OTU5NjEzNjQ1MzIxNTM1NDk4.Ykeb2Q.lKUTKAduDO_q1n1FA_qOrAEe1zM", Arrays.asList(intents))
+                .enableCache(CacheFlag.VOICE_STATE) // allows bot to detect voice channels
                 .setActivity(Activity.listening("lo-fi beats")) //sets the bot status
                 .addEventListeners(new Info()) //adds the info class to the main
                 .addEventListeners(new Help()) //adds the help class to the main
@@ -26,6 +34,7 @@ public class DiscordBot {
                 .addEventListeners(new Pomodoro()) //adds the pomodoro class to the main
                 .addEventListeners(new FortuneCookie()) //adds the FortuneCookie class to the main
                 .addEventListeners(new Coin()) // adds the Coin (flip a coin) class to the main
+                .addEventListeners(new Clear())
                 .build(); //builds instance and starts login process
     }
 }
